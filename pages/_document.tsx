b12f747b688da5a7d02a8f43e6b3b20319a786a2
krzysztof baranski <krisbaranski@gmail.com>
2023-04-28 21:22:14 +0200

    style: add font links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='preconnect'
+          href='../styles/Open_Sans/OpenSans-Italic-VariableFont_wdth,wght.ttf'
+        />
+        <link
+          rel='preconnect'
+          href='../styles/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
